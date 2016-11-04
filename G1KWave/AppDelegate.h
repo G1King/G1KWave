4ed74da1135bca29c1ebad902db6f2448dbccfcf
wangzhaoshuai <zhaoshuai.wang@bjsobf.com>
2016-11-04 11:21:43 +0800

    Initial Commit

---
+++ G1KWave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  G1KWave
+//
+//  Created by Sobf Leo on 16/11/4.
+//  Copyright © 2016年 SOBF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
